7332ec455ef8cd64a6c59955b8371f0a184f1bce
SubhankarSaho <subhankarsahoo19@gmail.com>
2023-07-15 18:29:20 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/6JYAt5NiZOQk1fEZ8T7SKd7cUkc14I4b",
+      accounts: [ "75e915ca17ad1026b69d364a50ad8fed7dfa5d5ab096d9156cc15e5903a23cb6" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
